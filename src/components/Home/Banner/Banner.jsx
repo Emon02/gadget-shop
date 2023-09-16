5905ab7addc0c24bb76b71625f2e95d381c88d12
emon02 <abdulalimemon11@gmail.com>
2023-09-16 23:20:55 +0600

    home page banner section

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,6 +1,19 @@
 import "./Banner.scss";
+import BannerImag from '../../../assets/banner-img.png'
 const Banner = () => {
-    return <div>Banner</div>;
+    return <div className="hero-banner">
+        <div className="content">
+            <div className="text-content">
+                <h1>SALES</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda reprehenderit magnam cumque atque voluptates consequatur?</p>
+                <div className="ctas">
+                    <button className="button-cta v1">Read More</button>
+                    <button className="button-cta v2">Shop Now</button>
+                </div>
+            </div>
+            <img className="banner-img" src={BannerImag} alt="Banner-Image" />
+        </div>
+    </div>;
 };
 
 export default Banner;
